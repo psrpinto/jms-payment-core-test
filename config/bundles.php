40e911a4df19bcc808a82d23f0f9c991edcb13e0
Paulo Rodrigues Pinto <regularjack@gmail.com>
2019-01-02 15:39:46 +0000

    Setup ORM

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-//    JMS\Payment\CoreBundle\JMSPaymentCoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
